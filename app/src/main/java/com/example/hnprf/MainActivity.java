6c9428b83ac635ed305cc8b9ca0f54052ffb8784
blacknight0712 <blacknight0981@gmail.com>
2024-03-29 10:52:01 +0800

    添加 activity_main 按鈕點擊回應

--- app/src/main/java/com/example/hnprf/MainActivity.java
+++ app/src/main/java/com/example/hnprf/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.hnprf;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toast toast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button connectButton = (Button) findViewById(R.id.activity_main_connect_button);
+        Button selectButton = (Button) findViewById(R.id.activity_main_select_button);
+        Button settingButton = (Button) findViewById(R.id.activity_main_setting_button);
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (toast != null) {toast.cancel();}
+
+
+                toast = Toast.makeText(MainActivity.this, "你點擊了 " + connectButton.getText()  , Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (toast != null) {toast.cancel();}
+
+                toast = Toast.makeText(MainActivity.this, "你點擊了 " + selectButton.getText()  , Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        settingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (toast != null) {toast.cancel();}
+
+                toast = Toast.makeText(MainActivity.this, "你點擊了 " + settingButton.getText()  , Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
+
+
 }
